efcda3ee64096968127146d18c772c46d408fa77
HeliXZz <helixzz@helixzz.com>
2020-05-12 17:23:46 +0800

    Update packing script

--- pack.sh
+++ pack.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Use makeself to pack as single self-extracting installer.
 makeself edgerouter-science-suite-v2 edgerouter-science-suite-v2.run "Science Suite v2 for EdgeRouter (MIPS64 platform)" ./install.sh
