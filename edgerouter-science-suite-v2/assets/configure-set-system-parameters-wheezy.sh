508e04d3af9ac5c542d913b935468d675ad768bd
HeliXZz <helixzz@helixzz.com>
2020-05-19 10:35:18 +0800

    Update system configuration script.

--- edgerouter-science-suite-v2/assets/configure-set-system-parameters-wheezy.sh
+++ edgerouter-science-suite-v2/assets/configure-set-system-parameters-wheezy.sh
@@ -4,6 +4,9 @@ source /opt/vyatta/etc/functions/script-template
 configure
 set system name-server 127.0.0.1
 set system name-server 114.114.114.114
+set system time-zone Asia/Shanghai
+delete system package repository
+set system package repository
 set system package repository wheezy
 set system package repository wheezy components "main contrib non-free"
 set system package repository wheezy distribution wheezy
